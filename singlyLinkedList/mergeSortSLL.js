5c874417b451ef96c393b8b514515104a2cb0a9e
Devon Langendoerfer <425devon@gmail.com>
2018-05-19 11:03:02 -0700

    cleaned up code for merge to master

--- singlyLinkedList/mergeSortSLL.js
+++ singlyLinkedList/mergeSortSLL.js
@@ -1,10 +1,10 @@
 const SLL = require('./SLL');
 let sll = new SLL();
 sll.insert(6)
-sll.insert(5)
-sll.insert(4)
-sll.insert(3)
+sll.insert(11)
 sll.insert(2)
+sll.insert(3)
+sll.insert(5)
 sll.insert(1)
 //
 
